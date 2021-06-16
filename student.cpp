4d33df0be7d2ff73ccec43145f561b841b8f4198
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-06-16 10:09:40 +0200

    Corrected student - files

--- student.cpp
+++ student.cpp
@@ -1,12 +1,45 @@
 #include "student.hpp"
 
-Student::Student (std::string name, std::string l_name, std::string adress, long long indexNumber, long long pesel, std::string gender)
-    : name_(name)
-    , l_name_(l_name)
-    , adress_(adress)
-    , indexNumber_(indexNumber)
-    , pesel_(pesel)
-    , gender_(gender) {} ;
+Student::Student(std::string name, std::string l_name, std::string adress, long long indexNumber, long long pesel, std::string gender)
+    : name_(name), l_name_(l_name), adress_(adress), indexNumber_(indexNumber), pesel_(pesel), gender_(gender){};
 
+void Student::setName(const std::string& name) {
+    name_ = name;
+}
+
+void Student::setLname(const std::string& l_name) {
+    l_name_ = l_name;
+}
+void Student::setAdress(const std::string& adress) {
+    adress_ = adress;
+}
+void Student::setIndex(const long long& indexNumber) {
+    indexNumber_ = indexNumber;
+}
+void Student::setPesel(const long long& pesel) {
+    pesel_ = pesel;
+}
+void Student::setGender(const std::string& gender) {
+    gender_ = gender;
+}
+
+std::string Student::getName() const {
+    return name_;
+}
+std::string Student::getLname() const {
+    return l_name_;
+}
+std::string Student::getAdress() const {
+    return adress_;
+}
+long long Student::getIndex() const {
+    return indexNumber_;
+}
+long long Student::getPesel() const {
+    return pesel_;
+}
+std::string Student::getGender() const {
+    return gender_;
+}
 //// Czy mam implementować konstruktor domyślny?
 // Nie rozumiem jak miałą by wyglądać druga klasa Uniwersity?
--- student.hpp
+++ student.hpp
@@ -1,12 +1,12 @@
 #pragma once
+
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm> 
-#include <fstream>
 
-class Student
-{
+class Student {
 private:
     std::string name_;
     std::string l_name_;
@@ -16,17 +16,21 @@ private:
     std::string gender_;
 
 public:
-    void addNewStudent();
-    void dispalayBase();
-    void findBySurname();
-    void findByPesel();
-    void sortByPesel();
-    void sortbySurname();
-    void removeByIndexNumber();
+    void setName(const std::string&);
+    void setLname(const std::string&);
+    void setAdress(const std::string&);
+    void setIndex(const long long&);
+    void setPesel(const long long&);
+    void setGender(const std::string&);
+    
+    std::string getName() const;
+    std::string getLname() const;
+    std::string getAdress() const;
+    long long getIndex() const;
+    long long getPesel() const;
+    std::string getGender() const;
+    
     Student() = default;
     Student(std::string name, std::string surname, std::string adress, long long indexNumber, long long pesel, std::string gender);
     ~Student();
-    //bool validationByPESEL();
-    //void writeToFIle();
-    //void readFile();
 };

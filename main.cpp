edac5b009e4a639eedd7febcdadf5e4b43e27d44
jantob6637 <83110306+jantob6637@users.noreply.github.com>
2021-06-17 12:49:25 +0200

    First commit
    Added first version "menu" and "choose" menu function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "univeristy.hpp"
 
 void menu();
+int wybor_menu(int);
+int wybor;
 
 int main() {
 
@@ -13,7 +15,42 @@ int main() {
     return 0;
 }
 
-void menu(){
-    std::cout<<"MENU"<<std::endl;
-    std::cout<<""<<std::endl;
+void menu() {
+   std::cout << "MENU GLOWNE:                      " <<std::endl;
+   std::cout << "----------------------------------" << std::endl;
+   std::cout << "1.Dodawanie nowych studentow      " << std::endl;
+   std::cout << "2.Wyświetlenie calej bazy daych   " << std::endl;
+   std::cout << "3.Wyszukiwanie po nazwisku        " << std::endl;
+   std::cout << "4.Wyszukiwanie po numerze pesel   " << std::endl;
+   std::cout << "5.Sortowanie po nuemrze pesel     " << std::endl;
+   std::cout << "6.Sortowanie po nazwisku          " << std::endl;
+   std::cout << "7.Usuwanie po numerze indeksu     " << std::endl;
+   std::cout << "8.Zakoncz                         " << std::endl;
+   std::cout << "Wybor: ";
 }
+int wybor_menu(int wybor) {
+    std::cin >> wybor;
+    switch (wybor)
+    {
+        case 1:
+    
+
+           break;
+        case 2:
+
+        case 3:
+
+        case 4:
+
+        case 5:
+
+        case 6:
+
+        case 7:
+
+        case 8:
+            exit(0);
+    }
+    return wybor;
+}
+

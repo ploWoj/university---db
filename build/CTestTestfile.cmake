c34b667f3026a98d043e481d1d5dda1ccca5e06b
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-07-28 16:50:47 +0200

    Removed build-dir

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martyna/kurs/university---db
-# Build directory: /home/martyna/kurs/university---db/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unitTests "./university---db-ut" "--success")
-set_tests_properties(unitTests PROPERTIES  _BACKTRACE_TRIPLES "/home/martyna/kurs/university---db/CMakeLists.txt;23;add_test;/home/martyna/kurs/university---db/CMakeLists.txt;0;")
